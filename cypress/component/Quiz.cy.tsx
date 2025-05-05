6a0761075348ee92a071ac846bb32152c0424331
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-05-05 10:46:12 -0500

    added comments to component test and built e2e test

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,48 +1,40 @@
-import Quiz from "../../client/src/components/Quiz"
+import Quiz from "../../client/src/components/Quiz";
 
-describe('Quiz Component', () => {
+describe("Quiz Component", () => {
   beforeEach(() => {
-    cy.intercept({
-        method: 'GET',
-        url: '/api/questions/random'
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/api/questions/random",
       },
       {
-        fixture: 'questions.json',
-        statusCode: 200
+        fixture: "questions.json",
+        statusCode: 200,
       }
-      ).as('getAQuestion')
-    });
-
-  it('should start my quiz & display my first question', () => {
-    cy.mount(<Quiz />);
-    cy.get('button').contains('Start Quiz').click();
-    cy.get('.card').should('be.visible');
-    cy.get('h2').should('not.be.empty');
+    ).as("getAQuestion");
   });
 
-  it('should answer my questions & complete my quiz', () => {
+  //test to see if quiz starts and displays first question
+  it("should start my quiz & display my first question", () => {
     cy.mount(<Quiz />);
-    cy.get('button').contains('Start Quiz').click();
-
-    // Answer questions
-    cy.get('button').contains('1').click();
-
-    // Verify the quiz completion
-    cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
+    cy.get("button").contains("Start Quiz").click();
+    cy.get(".card").should("be.visible");
+    cy.get("h2").should("not.be.empty");
   });
-
-  it('should restart my quiz when the game is done', () => {
+  //test to see if quiz answers questions and completes quiz
+  it("should answer my questions & complete my quiz", () => {
     cy.mount(<Quiz />);
-    cy.get('button').contains('Start Quiz').click();
-
-    // Answer questions
-    cy.get('button').contains('1').click();
-
-    // Restart the quiz
-    cy.get('button').contains('Take New Quiz').click();
-
-    // Verify the quiz is restarted
-    cy.get('.card').should('be.visible');
-    cy.get('h2').should('not.be.empty');
+    cy.get("button").contains("Start Quiz").click();
+    cy.get("button").contains("1").click();
+    cy.get(".alert-success").should("be.visible").and("contain", "Your score");
+  });
+  //test to see if quiz restarts when game is done
+  it("should restart my quiz when the game is done", () => {
+    cy.mount(<Quiz />);
+    cy.get("button").contains("Start Quiz").click();
+    cy.get("button").contains("1").click();
+    cy.get("button").contains("Take New Quiz").click();
+    cy.get(".card").should("be.visible");
+    cy.get("h2").should("not.be.empty");
   });
 });
