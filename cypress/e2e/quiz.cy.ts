df92c419654c0b33d18dad699309b53eb7396f6e
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-05-05 10:47:30 -0500

    added comments to quiz e2e

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -2,13 +2,13 @@ describe("Quiz Component", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-
+  //test to see if quiz starts and displays first question
   it("should start my quiz & display my first question", () => {
     cy.get("button").contains("Start Quiz").click();
     cy.get(".card").should("be.visible");
     cy.get("h2").should("not.be.empty");
   });
-
+  //test to see if quiz answers questions and completes quiz
   it("should answer my questions & complete my quiz", () => {
     cy.get("button").contains("Start Quiz").click();
     for (let i = 0; i < 10; i++) {
@@ -16,7 +16,7 @@ describe("Quiz Component", () => {
     }
     cy.get(".alert-success").should("be.visible").and("contain", "Your score");
   });
-
+  //test to see if quiz restarts when game is done
   it("should restart my quiz when the game is done", () => {
     cy.get("button").contains("Start Quiz").click();
     for (let i = 0; i < 10; i++) {
